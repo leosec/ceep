05329d4205f88e94dda44a07dea3d8a4383a8739
Leonardo Freitas <geeksleo@gmail.com>
2021-12-28 18:56:18 -0300

    Inicial

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import "./estilo.css";
+
+export default class FormularioCadastro extends Component {
+    
+    constructor(props){
+        super(props);
+        this.titulo = "";
+        this.texto = "";
+    }
+
+    _handleMudancaTitulo(evento){
+        evento.stopPropagation();
+        this.titulo = evento.target.value;
+    }
+    
+
+    _hendleMudancaTexto(evento){
+        evento.stopPropagation();
+        this.texto = evento.target.value;
+    }
+
+    _criarNota(evento){
+        evento.preventDefault();
+        evento.stopPropagation();
+        this.props.criarNota(this.titulo,this.texto);
+    }
+
+    render() {
+        return (
+            <form className="form-cadatro" onSubmit={this._criarNota.bind(this)}>
+                <input type="text" placeholder="Título" className="form-cadastro_input" onChange={this._handleMudancaTitulo.bind(this)} />
+                <textarea placeholder="Escreva sua nota..." rows={15} className="form-cadastro_input" onChange={this._hendleMudancaTexto.bind(this)}/>
+                <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
+            </form>
+        );
+    }
+}
