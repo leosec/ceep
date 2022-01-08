fdb28547213bb596cfbb5023064559335fbaa3e5
Leonardo Freitas <geeksleo@gmail.com>
2022-01-07 22:31:33 -0300

    Front-end - estilo lista de categorias e evento OnKeyUp

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -3,16 +3,30 @@ import "./estilo.css";
 
 class ListaDeCategorias extends Component {
 
+    
+    _handleEventoInput(e){
+        if(e.key == "Enter"){
+            return (console.log(e.target.value));
+        }
+    }
+
+    
     render() {
+
         return (
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
                     <li className="lista-categorias_item">Categoria 1</li>
-                    <li>Categoria 2</li>
-                    <li>Categoria 3</li>
-                    <li>Categoria 4</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 1</li>
                 </ul>
-                <input type="text" className="lista-categorias_input" placeholder="Crie uma nota..." />
+                <input
+                    type="text"
+                    className="lista-categorias_input"
+                    placeholder="Adcionar Categoria..."
+                    onKeyUp={this._handleEventoInput.bind(this)}
+                />
             </ section>
         );
     }
